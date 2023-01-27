04e88f5fc2111867646249ff02f6b55075663286
naekit <mykyta.onishchuck@code.berlin>
2023-01-27 13:33:41 -0500

    with lazy loader and suspense for blog post page

--- src/App.js
+++ src/App.js
@@ -1,32 +1,46 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { lazy, Suspense } from "react"
+// import BlogPage, { loader as postsLoader } from './pages/Blog';
+import HomePage from "./pages/Home"
+import PostPage, { loader as postLoader } from "./pages/Post"
+import RootLayout from "./pages/Root"
 
-import BlogPage, { loader as postsLoader } from './pages/Blog';
-import HomePage from './pages/Home';
-import PostPage, { loader as postLoader } from './pages/Post';
-import RootLayout from './pages/Root';
+const BlogPage = lazy(() => import("./pages/Blog"))
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <RootLayout />,
-    children: [
-      {
-        index: true,
-        element: <HomePage />,
-      },
-      {
-        path: 'posts',
-        children: [
-          { index: true, element: <BlogPage />, loader: postsLoader },
-          { path: ':id', element: <PostPage />, loader: postLoader },
-        ],
-      },
-    ],
-  },
-]);
+	{
+		path: "/",
+		element: <RootLayout />,
+		children: [
+			{
+				index: true,
+				element: <HomePage />,
+			},
+			{
+				path: "posts",
+				children: [
+					{
+						index: true,
+						element: (
+							<Suspense>
+								{" "}
+								<BlogPage />
+							</Suspense>
+						),
+						loader: () =>
+							import("./pages/Blog").then((module) =>
+								module.loader()
+							),
+					},
+					{ path: ":id", element: <PostPage />, loader: postLoader },
+				],
+			},
+		],
+	},
+])
 
 function App() {
-  return <RouterProvider router={router} />;
+	return <RouterProvider router={router} />
 }
 
-export default App;
+export default App
